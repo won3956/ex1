925ce860f51b880ff0031ebc006e228c3a0ea11e
jbg <asd@naver.com>
2024-05-03 14:34:09 +0900

    첫 프로젝트 설정 추가

---
+++ src/main/java/deu/ex11/Ex11.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package deu.ex11;
+
+/**
+ *
+ * @author 911-38
+ */
+public class Ex11 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("1");
+    }
+}
